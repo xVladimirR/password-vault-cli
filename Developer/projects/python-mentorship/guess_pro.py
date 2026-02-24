93d200cb70920d3e3acb2d9da8f3f5c805772fa7
Your Name <youremail@example.com>
2026-02-24 13:18:51 +0700

    Day 3: guessing game pro with loop

---
+++ Developer/projects/python-mentorship/guess_pro.py
@@ -0,0 +1,23 @@
+import random
+
+secret = random.randint(1, 10)
+attempts = 3
+
+print("Welcome to the guessing game!")
+print("You have 3 attempts.")
+
+while attempts > 0:
+    guess = int(input("Enter your guess (1, 10): "))
+
+    if guess == secret:
+        print("You win!")
+    if guess < secret:
+        print("Too low")
+    else:
+        print("Too high")
+
+    attempts -= 1
+    print(f"Attempts left {attempts}")
+
+if attempts == 0:
+    print(f"You lost. Number was {secret}")
